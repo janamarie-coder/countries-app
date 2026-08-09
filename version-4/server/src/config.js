f9daceccad2d5660b6d34bb8f3c7cd2e25153b8d
“janamarie-coder” <janaharms16@gmail.com>
2026-08-08 21:31:54 -0400

    use environment variable for database URL

--- version-4/server/src/config.js
+++ version-4/server/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  databaseUrl: "postgresql://neondb_owner:npg_mhcvCZ9fGrO6@ep-fragrant-glitter-ahptks8a-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  databaseUrl: process.env.DATABASE_URL,
 };
 
 export default config;
